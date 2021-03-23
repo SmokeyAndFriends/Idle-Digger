0a443702b28d024dfd2b3673e93856e2807ead4f
Ethan Belcher <65253767+SmokeyAndFriends@users.noreply.github.com>
2021-03-23 09:47:45 -0400

    Create clicker_code.js

---
+++ clicker_code.js
@@ -0,0 +1,23 @@
+//variables help the game increment clicks
+var ore = 0;
+var energy = 100;
+var stamina = 0.5; // stamina can be upgraded a total of 4 times
+var is_manager? = false
+
+//functions make the game work properly
+function mine_ore() {
+  
+  ore = ore + 1; 
+  energy = ore - stamina; //this equation is needed to help the upgrades work properly
+};
+
+function protocall_smc() {
+if (money => 150 + "$") {
+
+  money = money - 150;
+  is_manager? = true;
+  
+};
+
+
+
